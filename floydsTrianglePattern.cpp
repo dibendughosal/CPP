856484bb5caab178a6186b76744c5b6a0fd97bc8
Dibendu Ghosal <128916258+dibendughosal@users.noreply.github.com>
2024-09-06 18:44:34 +0530

    Floyd's Triangle Pattern

---
+++ floydsTrianglePattern.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int count;
+    cin>>count;
+    int num =1;
+    for (int i = 1; i <= count; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout<<num<<" ";
+            num++;
+        }
+        cout<<endl;
+        
+    }
+    
+}
